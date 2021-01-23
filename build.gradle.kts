a152cda9393da0ff470fc7b17e2afaf9236cac51
thomasdriscoll <thomasdriscoll98@gmail.com>
2021-01-23 09:26:35 -0600

    First commit; working config server

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("org.springframework.boot") version "2.4.2"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("java")
+}
+
+group = "com.thomas-driscoll"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_15
+
+
+repositories{
+    mavenCentral()
+}
+
+
+ext{
+    set("springCloudVersion", "2020.0.0")
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.cloud:spring-cloud-config-server:3.0.0")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
+    testImplementation("org.springframework.security:spring-security-test")
+    implementation("org.springdoc:springdoc-openapi-ui:1.4.8")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/test/java/com/thomasdriscoll/ares/AresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomasdriscoll.ares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
